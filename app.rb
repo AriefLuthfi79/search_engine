987415a446e1b01b52953301348656da7063e8a2
ariefluthfi79 <ariefluthfi799@gmail.com>
2019-09-22 06:00:46 +0700

    successfully add

--- app.rb
+++ app.rb
@@ -1,5 +1,8 @@
 require "open-uri"
 require "sinatra"
+require "sinatra/respond_with"
+require "sinatra/reloader"
+require "sinatra/json"
 require "uri"
 
 uri_target = "https://www.googleapis.com/customsearch/v1"
@@ -12,9 +15,14 @@ get '/' do
 	data_engine = JSON.parse(File.read("engine/engine_data.json"))
 
 	if params[:search]
-		query_string = {cx: data_engine["engine"]["cx"], q: "botak", key: data_engine["engine"]["key"]}
+		query_string = {cx: data_engine["engine"]["cx"], q: params[:search], key: data_engine["engine"]["key"]}
 		uri = URI.parse(uri_target)
 		uri.query = URI.encode_www_form(query_string)
-		uri.open.read
+		@json =  JSON.parse(uri.open.read)
+	end
+
+	respond_to do |format|
+		format.html { erb :search }
+		format.json { json @json }
 	end
 end
